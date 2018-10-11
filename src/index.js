1d4b60e9f509fe729f7b939d639e89f8ce84d30a
Clayton Salem <claytonjsalem@gmail.com>
2018-10-10 20:31:33 -0400

    Update styling

--- src/index.js
+++ src/index.js
@@ -39,43 +39,49 @@ class App extends React.Component {
 
     render() {
         return(
-            <div>
-                <form action="/">
-                <label htmlFor="firstname">First Name</label>
-                <input
-                    type="text"
-                    id="firstname"
-                    name="firstname"
-                    value={this.state.firstname}
-                    onChange={this.handleChange}
-                />
-                <label htmlFor="lastname">Last Name</label>
-                <input
-                    type="text"
-                    id="lastname"
-                    name="lastname"
-                    value={this.state.lastname}
-                    onChange={this.handleChange}
-                  />
-                <label htmlFor="nickname">Nick Name</label>
-                <input
-                    type="text"
-                    id="nickname"
-                    name="nickname"
-                    value={this.state.nickname}
-                    onChange={this.handleChange}
-                />
-                <label htmlFor="creditcard">Credit Card (not saved)</label>
-                <input
-                    type="text"
-                    id="creditcard"
-                    name="creditcard"
-                    value={this.state.creditcard}
-                    onChange={this.handleChange}
-                />
-                <button>Save</button>
+            <form action="/">
+                <div>
+                    <label htmlFor="firstname">First Name</label>
+                    <input
+                        type="text"
+                        id="firstname"
+                        name="firstname"
+                        value={this.state.firstname}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="lastname">Last Name</label>
+                    <input
+                        type="text"
+                        id="lastname"
+                        name="lastname"
+                        value={this.state.lastname}
+                        onChange={this.handleChange}
+                      />
+                </div>
+                <div>
+                    <label htmlFor="nickname">Nick Name</label>
+                    <input
+                        type="text"
+                        id="nickname"
+                        name="nickname"
+                        value={this.state.nickname}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="creditcard">Credit Card (not saved)</label>
+                    <input
+                        type="text"
+                        id="creditcard"
+                        name="creditcard"
+                        value={this.state.creditcard}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div><button>Save</button></div>
             </form>
-        </div>
         )
     }
 }
